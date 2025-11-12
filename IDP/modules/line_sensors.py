b6fe804e8f55713211500b73a9c9982efb10c168
Rohan Arora <arorarohan907@gmail.com>
2025-11-12 09:46:59 +0000

    refactor(line sensors): average data across samples

--- IDP/modules/line_sensors.py
+++ IDP/modules/line_sensors.py
@@ -2,9 +2,15 @@
 #NOTE: line sensor data comes in from a GPIO port and will be either logic HIGH or LOW. HIGH => light (on line) and LOW otherwise
 
 from machine import Pin
-from utime import sleep
 
 class LineSensors:
+
+    #static variables for data across samples
+    front_data = []
+    left_data = []
+    right_data = []
+    rear_data = []
+
     '''for collecting data from line sensors'''
     def __init__(self, front_pin, left_pin, right_pin, rear_pin) -> None: #collect port values for all sensors. Use GPXX number rather than jumper number
         #initialize pin instances from each sensor
@@ -13,15 +19,41 @@ class LineSensors:
         self.right_sensor = Pin(right_pin, Pin.IN, Pin.PULL_DOWN)
         self.rear_sensor = Pin(rear_pin, Pin.IN, Pin.PULL_DOWN)
 
-    def get_values(self) -> list:
-        '''Gets current values (0 or 1) from all 4 sensors and outputs as list [front, left, right, rear]'''
+    def get_new_values(self) -> None:
+        '''Appends current values (0 or 1) from all 4 sensors and adds to respective lists'''
         #poll sensors for values. 1 for white and 0 for black
-        front_value = self.front_sensor.value()
-        left_value = self.left_sensor.value()
-        right_value = self.right_sensor.value()
-        rear_value = self.rear_sensor.value()
+        self.front_data.append(self.front_sensor.value())
+        self.left_data.append(self.left_sensor.value())
+        self.right_data.append(self.right_sensor.value())
+        self.rear_data.append(self.rear_sensor.value())
+        return
         
-        # return list as per spec
-        values = [front_value, left_value, right_value, rear_value]
-        return values
-        
+    def get_averages(self) -> list[float]:
+        '''average the values from all the sensors and clear data, call this after a certain number of loops of get_new_values'''
+        #error handling for if we try to average an empty list
+        if self.front_data == [] or self.left_data == [] or self.right_data == [] or self.rear_data == []:
+            raise Exception("sensors have not collected any data")
+
+        else: #main averaging
+            front_sum = 0
+            for x in self.front_data:
+                front_sum += x
+            front_average = front_sum / len(self.front_data)
+
+            left_sum = 0
+            for x in self.left_data:
+                left_sum += x
+            left__average = left_sum / len(self.left_data)
+
+            right_sum = 0
+            for x in self.right_data:
+                right_sum += x
+            right_average = right_sum / len(self.right_data)
+
+            rear_sum = 0
+            for x in self.rear_data:
+                rear_sum += x
+            rear_average = rear_sum / len(self.rear_data)
+
+            #now return the averages
+            return [front_average, left__average, right_average, rear_average]
